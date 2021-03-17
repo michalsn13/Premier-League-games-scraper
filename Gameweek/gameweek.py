0850ef6206a1ebb3f633ffd65e64d6d787309805
Michał <78501697+michalsn13@users.noreply.github.com>
2021-03-17 19:52:14 +0100

    Update gameweek.py

--- Gameweek/gameweek.py
+++ Gameweek/gameweek.py
@@ -1,17 +1,16 @@
-import requests
 from .gameweek_locators import Gameweek_locators
 from Game.game import Game
 from bs4 import BeautifulSoup
 class Gameweek:
-    def __init__(self,number:int):
+    def __init__(self,number:int,content):
         if number>38 or number<1:
             raise Exception('No such gameweek.')
         self.number=number
+        self.content=content
     def __repr__(self):
         return f'Gameweek {self.number}'
     @property
     def games(self):
-        code = requests.get(f'https://www.worldfootball.net/schedule/eng-premier-league-2020-2021-spieltag/{self.number}/')
-        soup = BeautifulSoup(code.text, 'html.parser')
+        soup = BeautifulSoup(self.content, 'html.parser')
         rawgames = soup.select_one(Gameweek_locators.TABLE)
         return [Game(i,self.number) for i in rawgames.select(Gameweek_locators.SEPARATOR)]
